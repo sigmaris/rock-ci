6dd73fd1f59789e424a8edc7c8620f489249e797
Hugh Cole-Baker <sigmaris@gmail.com>
2022-03-20 16:07:45 +0000

    build caching for tf-a

--- scripts/build_tfa.sh
+++ scripts/build_tfa.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 set -euo pipefail
 
-cd "${WORKDIR}/arm-trusted-firmware"
+print_joined() {
+    local IFS="$1"
+    shift
+    echo "$*"
+}
 
-make realclean
-
-MAKEFLAGS=("-j$(getconf _NPROCESSORS_ONLN)" CROSS_COMPILE=aarch64-linux-gnu- PLAT=rk3399)
+MAKEFLAGS=(CROSS_COMPILE=aarch64-linux-gnu- PLAT=rk3399)
 RESULT_DIR=release
 if [[ -n "${DEBUG-}" ]]
 then
     MAKEFLAGS+=("DEBUG=1")
     RESULT_DIR=debug
 fi
-make "${MAKEFLAGS[@]}" bl31
 
-echo "::set-output name=bl31::$(pwd)/build/rk3399/${RESULT_DIR}/bl31/bl31.elf"
+case "${1:-}" in
+get-cache-key)
+    print_joined _ "${MAKEFLAGS[@]}"
+    ;;
+*)
+    cd "${WORKDIR}/arm-trusted-firmware"
+    if [[ ! -f "build/rk3399/${RESULT_DIR}/bl31/bl31.elf" || -n "${NO_BUILD_CACHE:-}" ]]
+    then
+        make realclean
+        make "-j$(getconf _NPROCESSORS_ONLN)" "${MAKEFLAGS[@]}" bl31
+    fi
+    echo "::set-output name=bl31::$(pwd)/build/rk3399/${RESULT_DIR}/bl31/bl31.elf"
+    ;;
+esac
--- scripts/clone_tfa.sh
+++ scripts/clone_tfa.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-cd "${WORKDIR}"
-
-git clone --depth 1 --branch "$TF_A_REF" -- "$TF_A_REPO" "arm-trusted-firmware"
+case "${1:-}" in
+get-cache-key)
+    git ls-remote --quiet --exit-code "$TF_A_REPO" "$TF_A_REF" | head -n 1 | awk '{print $1;}'
+    ;;
+*)
+    cd "${WORKDIR}"
+    git clone --depth 1 --branch "$TF_A_REF" -- "$TF_A_REPO" "arm-trusted-firmware"
+    ;;
+esac
