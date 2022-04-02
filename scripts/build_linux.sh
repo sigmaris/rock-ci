14baf7d917a7d090e024d53a4011b98daded94e6
Hugh Cole-Baker <sigmaris@gmail.com>
2022-04-02 12:40:51 +0000

    cache Debian image layer

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -6,16 +6,17 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 
+cd "$WORKDIR"
+
 if compgen -G "linux-image-*_${ARCH}.deb" > /dev/null && [[ -z "${NO_BUILD_CACHE:-}" ]]
 then
     # Use cached build
-    cd "$WORKDIR"
     LINUX_IMAGE_DEB="$(find . -type f -name "linux-image-*_${ARCH}.deb" -not -name 'linux-image-*-dbg_*_${ARCH}.deb' | head -n 1)"
     VERSION_PART="${LINUX_IMAGE_DEB#linux-image-}"
     KDEB_PKGVERSION="${VERSION_PART%_*_${ARCH}.deb}"
 else
     # Build Linux
-    cd "${WORKDIR}/linux"
+    cd linux
     make mrproper
     echo "-g$(git rev-parse --short HEAD)" > .scmversion
     cp "${SCRIPTPATH}/../configs/rockpro64_linux_defconfig" arch/${ARCH}/configs/rockpro64_linux_defconfig
